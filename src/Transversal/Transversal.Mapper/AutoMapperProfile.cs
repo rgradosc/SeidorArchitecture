d637f1488226b4ed545897d44c34843ccac58ae1
Raúl Grados Carreño <gnr2092@gmail.com>
2021-03-02 23:32:18 -0500

    Configura el mapeo de nuevos objetos
    Se configura el perfil de automapper para transformar los DTOs a las entidades.

--- src/Transversal/Transversal.Mapper/AutoMapperProfile.cs
+++ src/Transversal/Transversal.Mapper/AutoMapperProfile.cs
@@ -9,6 +9,24 @@ namespace SeidorArchitecture.ECommerce.Transversal.Mapper
         public AutoMapperProfile()
         {
             CreateMap<Customer, CustomerDTO>().ReverseMap();
+            CreateMap<Customer, AddCustomerDTO>().ReverseMap();
+            CreateMap<CreditCustomerDTO, Customer>()
+                .ForMember(x => x.Puntos, options => options.MapFrom(MapAbonoPuntos))
+                .ForMember(x => x.Saldo, options => options.MapFrom(y => y.Abono));
+        }
+
+        private double MapAbonoPuntos(CreditCustomerDTO creditCustomerDTO, Customer customer)
+        {
+            if (creditCustomerDTO.Abono >= 1000)
+            {
+                customer.Puntos = 200;
+            }
+            else
+            {
+                customer.Puntos = 50;
+            }
+
+            return customer.Puntos;
         }
     }
 }
