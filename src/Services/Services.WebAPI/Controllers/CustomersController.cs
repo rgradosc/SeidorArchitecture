2680f4238c052e9c068ed7214c31e869040bb4c5
Raúl Grados Carreño <gnr2092@gmail.com>
2021-03-02 19:06:53 -0500

    Agrega el web api
    Agregado el Web Api basado en rest de la aplicacion.

---
+++ src/Services/Services.WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,195 @@
+﻿using SeidorArchitecture.ECommerce.Application.DTO;
+using SeidorArchitecture.ECommerce.Application.Interfaces;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerApplication application;
+
+        public CustomersController(ICustomerApplication application)
+        {
+            this.application = application;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var response = application.GetAll();
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet("{customerId}")]
+        public IActionResult Get(string customerId)
+        {
+            if (string.IsNullOrWhiteSpace(customerId))
+            {
+                return BadRequest();
+            }
+
+            var response = application.Get(customerId);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CustomerDTO customerDTO)
+        {
+            if (customerDTO == null)
+            {
+                return BadRequest();
+            }
+
+            var response = application.Insert(customerDTO);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] CustomerDTO customerDTO)
+        {
+            if (customerDTO == null)
+            {
+                return BadRequest();
+            }
+
+            var response = application.Update(customerDTO);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpDelete("{customerId}")]
+        public IActionResult Delete(string customerId)
+        {
+            if (string.IsNullOrWhiteSpace(customerId))
+            {
+                return BadRequest();
+            }
+
+            var response = application.Delete(customerId);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var response = await application.GetAllAsync();
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetAsync(string customerId)
+        {
+            if (string.IsNullOrWhiteSpace(customerId))
+            {
+                return BadRequest(customerId);
+            }
+
+            var response = await application.GetAsync(customerId);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] CustomerDTO customerDTO)
+        {
+            if (customerDTO == null)
+            {
+                return BadRequest();
+            }
+
+            var response = await application.InsertAsync(customerDTO);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync([FromBody] CustomerDTO customerDTO)
+        {
+            if (customerDTO == null)
+            {
+                return BadRequest();
+            }
+
+            var response = await application.UpdateAsync(customerDTO);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpDelete("{customerId}")]
+        public async Task<IActionResult> DeleteAsync(string customerId)
+        {
+            if (string.IsNullOrWhiteSpace(customerId))
+            {
+                return BadRequest();
+            }
+
+            var response = await application.DeleteAsync(customerId);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+    }
+}
---
+++ src/Services/Services.WebAPI/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SeidorArchitecture.ECommerce.Application.Interfaces;
+using SeidorArchitecture.ECommerce.Application.Main;
+using SeidorArchitecture.ECommerce.Domain.Core;
+using SeidorArchitecture.ECommerce.Domain.Interfaces;
+using SeidorArchitecture.ECommerce.Infrastructure.Data;
+using SeidorArchitecture.ECommerce.Infrastructure.Interfaces;
+using SeidorArchitecture.ECommerce.Infrastructure.Repository;
+using SeidorArchitecture.ECommerce.Transversal.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddSingleton<IConfiguration>(Configuration);
+
+            services.AddSingleton<IConnectionFactory, ConnectionFactory>();
+
+            services.AddScoped<ICustomerApplication, CustomerApplication>();
+
+            services.AddScoped<ICustomerDomain, CustomerDomain>();
+
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+            services.AddControllers()
+                .AddNewtonsoftJson();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
