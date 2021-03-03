d4f97a74bcd1e4f2b7f3af1e1af8cc13ea5db4ec
Raúl Grados Carreño <gnr2092@gmail.com>
2021-03-02 19:08:30 -0500

    Agrega el proyecto de testing

---
+++ testing/UnitTests/BasePruebas.cs
@@ -0,0 +1,13 @@
+﻿using SeidorArchitecture.ECommerce.Application.Interfaces;
+using SeidorArchitecture.ECommerce.Application.Main;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    public class BasePruebas
+    {
+        
+    }
+}
---
+++ testing/UnitTests/CustomersControllerTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CustomersControllerTests
+    {
+        [TestMethod]
+        public void ObtenerTodosLosClientes()
+        {
+            // preparacion
+
+
+        }
+    }
+}
